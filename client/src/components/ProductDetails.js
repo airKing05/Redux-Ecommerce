f5bf65254df22578ed08505fc9a1aa94b975cc5f
anilraj <anil050797@gmial.com>
2021-10-02 02:17:54 +0530

    details page is on going

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -5,6 +5,18 @@ import axios from 'axios';
 
 export default function ProductDetails() {
     const {productId} = useParams();
+
+    const fetchProductDetail = async () => {
+        try {
+            const response = await axios.get(`https://fakestoreapi.com/products/${id}`)
+            console.log('single product details', response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        fetchProductDetail();
+    }, [])
     console.log(productId)
     return (
         <div>
