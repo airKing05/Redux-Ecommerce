7e6690c0ee805f1aaf0d962654281a5861a40463
airKing05 <anil050797@gmail.com>
2022-07-25 13:48:07 +0530

    screenshot

--- client/src/components/ProductListing.js
+++ client/src/components/ProductListing.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SingleProduct from './SingleProduct';
-import { fetchProducts} from '../redux/actions/productActions';
+import { fetchProducts } from '../redux/actions/productActions';
 
 export default function ProductListing() {
     const products = useSelector((state) => state);
@@ -12,15 +12,10 @@ export default function ProductListing() {
     }, [])
     console.log("product: ", products)
 
- 
+
     return (
         <div className="container ">
-            <div className="row">
-                <div className="col-md-4 d-flex flex-wrap">
-                <SingleProduct/>
-                </div>
-            </div>
-           
+            <SingleProduct />
         </div>
     )
 }
--- client/src/components/SingleProduct.js
+++ client/src/components/SingleProduct.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 
 export default function SingleProduct() {
@@ -7,27 +7,27 @@ export default function SingleProduct() {
     // console.log(products)
     // const {id, title, category} = products[0];
     return (
-        <div>
+        <div className='row d-flex justify-content-center mt-5 cursor-pointer  '>
             {
-                products.map((product)=>{
-                    const {id, title, price, category, image} = product;
+                products.map((product) => {
+                    const { id, title, price, category, image } = product;
 
-                    return( 
-                        <div className="container border rounded mx-2 my-2 px-2 py-2  d-flex justify-content-start cursor-pointer " style={{maxWidth: '270px'}} key={id}>
-                             <Link to={`/product/${id}`}>
-                        <div className="row">
-                            <div className="col text-center">
-                              <img src={image} alt="single-product-img" style={{maxWidth: '150px'}}/>
-                                <h4>{title} </h4>
-                                <h5>{price}</h5>
-                                <p>{category}</p>
-                            </div>
+                    return (
+                        <div className="col-md-4 border rounded mx-2 my-2 px-2 py-2 " style={{ maxWidth: '270px' }} key={id}>
+                            <Link to={`/product/${id}`}>
+                                <div className="row">
+                                    <div className="col text-center">
+                                        <img src={image} alt="single-product-img" style={{ maxWidth: '150px' }} />
+                                        <h4>{title} </h4>
+                                        <h5>{price}</h5>
+                                        <p>{category}</p>
+                                    </div>
+                                </div>
+                            </Link>
                         </div>
-                        </Link>
-                    </div> 
                     )
                 })
-            }    
+            }
         </div>
     )
 }
